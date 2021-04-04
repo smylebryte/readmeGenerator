52dc2c768bb9ffa6ea968b8415d4626ad3fe96fa
Smylebryte <aliciahughes1446@gmail.com>
2021-04-03 22:53:26 -0500

    added additional questions to write to generateMarkdown

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#Hello
+
+  ## Table of Contents
+
+
+  ## Title
+  Alicia 
+
+  ## Description
+  some project I made 
+
+  ## Installation
+  npm i 
+
+  ## Usage
+  some things 
+
+  ## License
+  GPL 3.0 
+
+  ## Contributing
+  nothing 
+
+  ## Tests
+  npm test 
+  
+  ## Questions
+  smylebryte
+  alicia.smiley417@hotmail.com
+
--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
     {
         type: 'input',
+        name: 'title',
+        message: "What is the title of your project?"
+    },
+    {
+        type: 'input',
         name: 'username',
         message: "What is your Github username?"
     },
@@ -15,23 +20,48 @@ const questions = [
         type: 'input',
         name: 'email',
         message: "What is your email address?"
-    }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please write a short description of your project?"
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "What kind of license should your project have?"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // let testMarkdown = "#Hello"
-    // return fs.writeFileSync("test.md", testMarkdown)
-    // return fs.writeFileSync(path.join(process.cwd(), fileName), data)
-    let testMarkdown = "#Hello"
-    return fs.writeFileSync(path.join(process.cwd(), "README.md"), testMarkdown)
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    // writeToFile("test2.md", "blabla")
     inquirer.prompt(questions).then((responses) => {
-        console.log(responses)
+        console.log("Generating Readme")
         writeToFile("README.md", generateMarkdown({ ...responses }))
     })
 }
